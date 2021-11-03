c06028098afda249579ab38c834f9dd1d0888d4e
Omar <omar@kamelizer.com>
2021-11-03 10:48:25 +0200

    adjust nats ack mode to manual

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,13 +10,26 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
     
-    const subscription = stan.subscribe('ticket:created');
+    const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true) // acknowledment mode to manual
+
+    const subscription = stan.subscribe(
+        'ticket:created', // channel we are subscribing to
+        'orders-service-queue-group', // queue group we subscribe to 
+        options
+        );
 
     subscription.on('message', (msg : Message) => {
+        // get data from the message
         const data = msg.getData();
 
+        // make sure the data of the message recieved is of type string
         if(typeof data === 'string'){
             console.log(`Received event#${msg.getSequence()}, with data: ${data}`);
         }
+
+        // acknowledge that the message is recieved
+        msg.ack();
     })
 })

cfd213fb8e1b30ab14a8b5dd74bd9f3362412edf
Omar <omar@kamelizer.com>
2021-11-05 16:13:58 +0200

    add base listener and base publisher, enhance code

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
+import {Listener} from './events/base-listener'
 import { randomBytes } from 'crypto'
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,28 +17,7 @@ stan.on('connect', () => {
         process.exit();
     })
     
-    const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true) // acknowledment mode to manual
-
-    const subscription = stan.subscribe(
-        'ticket:created', // channel we are subscribing to
-        'orders-service-queue-group', // queue group we subscribe to 
-        options
-        );
-
-    subscription.on('message', (msg : Message) => {
-        // get data from the message
-        const data = msg.getData();
-
-        // make sure the data of the message recieved is of type string
-        if(typeof data === 'string'){
-            console.log(`Received event#${msg.getSequence()}, with data: ${data}`);
-        }
-
-        // acknowledge that the message is recieved
-        msg.ack();
-    })
+    new TicketCreatedListener(stan);
 })
 
 process.on('SIGINT', () => stan.close())
